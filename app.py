f768b5ab6f281f630bc73fd5497fe3dc0bc75a14
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-08-07 23:26:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit_chat import message
 from hugchat import hugchat
 from hugchat.login import Login
-
 #from streamlit_extras.colored_header import colored_header
 #from streamlit_extras.add_vertical_space import add_vertical_space
 
