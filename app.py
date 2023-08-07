6a137acbbaf99dbef8b8d9e6e0601fdc2dd71c03
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-08-07 15:56:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def generate_response(prompt_input, email, passwd):
 
 # User-provided prompt
 #if prompt := st.chat_input(disabled=not (hf_email and hf_pass)): 
-if prompt = st.chat_input(disabled=not (hf_email and hf_pass)):
+if prompt == st.chat_input(disabled=not (hf_email and hf_pass)):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
